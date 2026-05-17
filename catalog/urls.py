0514e412d109363c72b99e19355f2972d789e21a
Суспицын Евгений <evgen.es21@gmail.com>
2026-05-17 22:19:14 +0400

    fix

---
+++ catalog/urls.py
@@ -0,0 +1,9 @@
+from tkinter.font import names
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('/', views.home, name='home'),
+    path('contacts/', views.contacts, name='contacts')
+]
